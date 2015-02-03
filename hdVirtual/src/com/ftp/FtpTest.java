069bb40ccccb7e20b2d8044dbe5405efbab8bc6a
braynner <max.braynner@gmail.com>
2015-02-02 21:57:01 -0300

    Commit inicial

--- hdVirtual/src/com/ftp/FtpTest.java
+++ hdVirtual/src/com/ftp/FtpTest.java
@@ -6,7 +6,8 @@ import java.io.IOException;
 public class FtpTest {
 
     public static void main(String[] args) throws IOException {
-        ClienteFTP cl = new ClienteFTP();
+        
+    	ClienteFTP cl = new ClienteFTP();
         cl.conect("ftp.xpg.com.br", 21);
         cl.login("lucasolvmacedo", "59236094");
         //cl.CWD("Imagens");
